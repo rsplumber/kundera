ed7d3e5d03d3ce6ee4c8b8f666d2c3947b84609f
Aliasghar Sharifian(Plumber) <sha.a.wf@gmail.com>
2022-09-12 07:56:38 +0430

    Init project

---
+++ Users/Users.Domain/User.cs
@@ -0,0 +1,10 @@
+﻿using Tes.Domain.Contracts;
+
+namespace Users.Domain;
+
+public class User : AggregateRoot<UserId>
+{
+    public User(UserId id) : base(UserId.Generate())
+    {
+    }
+}
---
+++ Users/Users.Domain/UserId.cs
@@ -0,0 +1,8 @@
+﻿using Tes.Domain.Contracts;
+
+namespace Users.Domain;
+
+public class UserId : CustomType<Guid, UserId>, IIdentity
+{
+    public static UserId Generate() => From(Guid.NewGuid());
+}
