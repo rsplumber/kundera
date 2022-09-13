5228452dfd41dd1c8c728c13fbb55398ebfb6fd2
s.kalami <s.kalami@tes.ir>
2022-09-13 09:44:54 +0430

    feature : UserStatus added to User domain.

--- Users/Users.Domain/Users/Types/NationalCode.cs
+++ Users/Users.Domain/Users/Types/NationalCode.cs
@@ -6,7 +6,7 @@ namespace Users.Domain.Users.Types;
 
 public class NationalCode : CustomType<string, NationalCode>
 {
-    private const string RegexPattern = "^(?!(\\d)\\1{9})\\d{10}$";
+    private const string RegexPattern = @"^(?!(\d)\1{9})\d{10}$";
 
     public static implicit operator string(NationalCode nationalCode) => nationalCode.Value;
 
@@ -15,6 +15,14 @@ public class NationalCode : CustomType<string, NationalCode>
     protected override void Validate()
     {
         if (!Regex.IsMatch(Value, RegexPattern, RegexOptions.IgnoreCase))
+            throw new InvalidNationalCodeException(Value);
+
+        var check = Convert.ToInt32(Value.Substring(9, 1));
+        var sum = Enumerable.Range(0, 9)
+            .Select(x => Convert.ToInt32(Value.Substring(x, 1)) * (10 - x))
+            .Sum() % 11;
+
+        if (!(sum < 2 ? check == sum : check + sum == 11))
         {
             throw new InvalidNationalCodeException(Value);
         }
