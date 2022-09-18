1588bae68756634fd3544d99b61ac83443c4b9c2
s.kalami <s.kalami@tes.ir>
2022-09-18 11:24:17 +0430

    feature: userGroup command added.

---
+++ Users/Users.Application/UserGroups/UserGroupRoleCommand.cs
@@ -0,0 +1,8 @@
+﻿using Tes.CQRS.Contracts;
+using Users.Domain;
+
+namespace Users.Application.UserGroups;
+
+public sealed record AssignUserGroupRoleCommand(params RoleId[] Roles) : Command;
+
+public sealed record RevokeUserGroupRoleCommand(params RoleId[] Roles) : Command;
