5b3cba48c8487df6a29045af6a08443a6a6cae21
s.kalami <s.kalami@tes.ir>
2022-09-18 12:52:14 +0430

    feature: User quries added.

---
+++ Users/Users.Application/Users/UsersQuery.cs
@@ -0,0 +1,23 @@
+﻿using Tes.CQRS.Contracts;
+using Users.Domain.Users;
+
+namespace Users.Application.Users;
+
+public sealed record UsersQuery() : Query<IEnumerable<UserResponse>>;
+
+public sealed record UsersResponse(string Id)
+{
+    
+    public string? Firstname { get; set; }
+    
+    public string? Lastname { get; set; }
+    
+    public string? Username { get; set; }
+    
+    public string? PhoneNumber { get; set; }
+    
+    public string? Email { get; set; }
+    
+    public string? NationalCode { get; set; }
+
+}
