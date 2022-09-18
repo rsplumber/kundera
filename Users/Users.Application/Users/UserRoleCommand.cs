25b38e8d8d3761a005c2af9390d3f8b3f1fd2199
s.kalami <s.kalami@tes.ir>
2022-09-18 11:34:12 +0430

    bug: userId and UserGroupId added to required commands.

--- Users/Users.Application/Users/UserRoleCommand.cs
+++ Users/Users.Application/Users/UserRoleCommand.cs
@@ -1,9 +1,9 @@
 ﻿using Tes.CQRS.Contracts;
 using Users.Domain;
-using Users.Domain.UserGroups;
+using Users.Domain.Users;
 
 namespace Users.Application.Users;
 
-public sealed record AssignUserRoleCommand(params RoleId[] Roles) : Command;
+public sealed record AssignUserRoleCommand(UserId UserId, params RoleId[] Roles) : Command;
 
-public sealed record RevokeUserRoleCommand(params RoleId[] Roles) : Command;
+public sealed record RevokeUserRoleCommand(UserId UserId, params RoleId[] Roles) : Command;
