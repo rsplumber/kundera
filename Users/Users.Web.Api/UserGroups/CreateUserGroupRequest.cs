c1eced6e38535fcb213fe9ae0359599c96d50a44
s.kalami <s.kalami@tes.ir>
2022-09-19 09:51:56 +0430

    feature : role management added to UserGroupsController.

--- Users/Users.Web.Api/UserGroups/CreateUserGroupRequest.cs
+++ Users/Users.Web.Api/UserGroups/CreateUserGroupRequest.cs
@@ -1,28 +1,21 @@
 ﻿using FluentValidation;
 using Tes.Web.Validators;
 using Users.Application.UserGroups;
-using Users.Domain;
 
 namespace Users.Web.Api.UserGroups;
 
-public record CreateUserGroupRequest(string Name, List<string> RoleIds) : IWebRequest
+public record CreateUserGroupRequest(string Name) : IWebRequest
 {
-    public CreateUserGroupCommand ToCommand() => new(Name, RoleIds.Select(RoleId.From).ToArray());
+    public CreateUserGroupCommand ToCommand() => new(Name);
 }
 
 public class CreateUserGroupRequestValidator : RequestValidator<CreateUserGroupRequest>
 {
     public CreateUserGroupRequestValidator()
     {
-        //Todo Enter a valid Email???(Message)
         RuleFor(request => request.Name)
-            .MinimumLength(3).WithMessage("UserGroup minimum length 3")
+            .MinimumLength(6).WithMessage("Email minimum length 3")
             .NotEmpty().WithMessage("Enter a valid Email")
             .NotNull().WithMessage("Enter a valid Email");
-
-        //Todo Enter a valid UserGroup???(Message)
-        RuleFor(request => request.RoleIds)
-            .NotEmpty().WithMessage("Enter a valid UserGroup")
-            .NotNull().WithMessage("Enter a valid UserGroup");
     }
 }
