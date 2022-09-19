29334c38c9c00bbb0b80cd5139e6a65dbaf989ac
s.kalami <s.kalami@tes.ir>
2022-09-19 15:34:38 +0430

    bug: user query name fixed.

--- Users/Users.Data/UserGroups/UserGroupsQueryHandler.cs
+++ Users/Users.Data/UserGroups/UserGroupsQueryHandler.cs
@@ -3,7 +3,7 @@ using Users.Application.UserGroups;
 
 namespace Users.Data.UserGroups;
 
-internal sealed class UsersGroupsQueryHandler : QueryHandler<UserGroupsQuery, IEnumerable<UserGroupsResponse>>
+internal sealed class UserGroupsQueryHandler : QueryHandler<UserGroupsQuery, IEnumerable<UserGroupsResponse>>
 {
     public override Task<IEnumerable<UserGroupsResponse>> HandleAsync(UserGroupsQuery message, CancellationToken cancellationToken = default)
     {
