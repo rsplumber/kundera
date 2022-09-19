11b430eced3e9f8d958ac43ce84abf8f1d13ca8b
s.kalami <s.kalami@tes.ir>
2022-09-19 09:06:12 +0430

    feature: UserGroupsController added.

---
+++ Users/Users.Web.Api/UserGroups/CreateUserGroupRequest.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Tes.Web.Validators;
+using Users.Application.UserGroups;
+using Users.Domain;
+
+namespace Users.Web.Api.UserGroups;
+
+public record CreateUserGroupRequest(string Name, List<string> RoleIds) : IWebRequest
+{
+    public CreateUserGroupCommand ToCommand() => new(Name, RoleIds.Select(RoleId.From).ToArray());
+}
+
+public class CreateUserGroupRequestValidator : RequestValidator<CreateUserGroupRequest>
+{
+    public CreateUserGroupRequestValidator()
+    {
+        RuleFor(request => request.Name)
+            .MinimumLength(3).WithMessage("UserGroup minimum length 3")
+            .NotEmpty().WithMessage("Enter a valid Email")
+            .NotNull().WithMessage("Enter a valid Email");
+
+        RuleFor(request => request.RoleIds)
+            .NotEmpty().WithMessage("Enter a valid UserGroup")
+            .NotNull().WithMessage("Enter a valid UserGroup");
+    }
+}
---
+++ Users/Users.Web.Api/UserGroups/UserGroupsController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Tes.CQRS;
+using Users.Application.UserGroups;
+using Users.Application.Users;
+using Users.Domain.UserGroups;
+using Users.Domain.Users;
+using Users.Web.Api.Users;
+
+namespace Users.Web.Api.UserGroups;
+
+[ApiController]
+[Route("/user-groups")]
+public class UserGroupsController : ControllerBase
+{
+    private readonly IServiceBus _serviceBus;
+
+    public UserGroupsController(IServiceBus serviceBus)
+    {
+        _serviceBus = serviceBus;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody] CreateUserGroupRequest request, CancellationToken cancellationToken)
+    {
+        var command = request.ToCommand();
+        await _serviceBus.SendAsync(command, cancellationToken);
+        return Ok();
+    }
+    
+
+    [HttpGet]
+    public async Task<IActionResult> UserGroupsAsync([FromQuery] string? name, CancellationToken cancellationToken)
+    {
+        var query = new UserGroupsQuery();
+        var response = await _serviceBus.QueryAsync(query, cancellationToken);
+        return Ok(response);
+    }
+
+    [HttpGet("{id:required}")]
+    public async Task<IActionResult> UserGroupAsync([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var query = new UserGroupQuery(UserGroupId.From(id));
+        var response = await _serviceBus.QueryAsync(query, cancellationToken);
+        return Ok(response);
+    }
+}
