17908148771b421e136ba8fae5255a85efafd32c
s.kalami <s.kalami@tes.ir>
2022-10-10 16:08:44 +0330

    bugfix : add user check userGroup exists bug fixed.

--- Application/Users/CreateUserCommand.cs
+++ Application/Users/CreateUserCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.UserGroups;
+using Domain.UserGroups.Exception;
 using Domain.Users;
 using Tes.CQRS;
 using Tes.CQRS.Contracts;
@@ -10,15 +11,23 @@ public sealed record CreateUserCommand(Username Username, UserGroupId UserGroup)
 internal sealed class CreateUserCommandHandler : CommandHandler<CreateUserCommand>
 {
     private readonly IUserRepository _userRepository;
+    private readonly IUserGroupRepository _userGroupRepository;
 
-    public CreateUserCommandHandler(IUserRepository userRepository)
+    public CreateUserCommandHandler(IUserRepository userRepository, IUserGroupRepository userGroupRepository)
     {
         _userRepository = userRepository;
+        _userGroupRepository = userGroupRepository;
     }
 
     public override async Task HandleAsync(CreateUserCommand message, CancellationToken cancellationToken = default)
     {
         var (username, userGroupId) = message;
+        var userGroup = await _userGroupRepository.FindAsync(message.UserGroup, cancellationToken);
+        if (userGroup is null)
+        {
+            throw new UserGroupNotFoundException();
+        }
+
         var user = await User.CreateAsync(username, userGroupId, _userRepository);
         await _userRepository.AddAsync(user, cancellationToken);
     }
