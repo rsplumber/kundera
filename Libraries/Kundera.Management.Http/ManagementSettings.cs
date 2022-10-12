1e26e9db33ec23aadde401776c874042919c3076
Aliasghar Sharifian(Plumber) <sha.a.wf@gmail.com>
2022-10-12 11:18:29 +0330

    feature: Kundera.Management.Http added

---
+++ Libraries/Kundera.Management.Http/ManagementSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Kundera.Management.Http;
+
+internal sealed class ManagementSettings
+{
+    public string BaseUrl { get; set; } = string.Empty;
+}
--- Libraries/Kundera.Management/Users/UserResponse.cs
+++ Libraries/Kundera.Management/Users/UserResponse.cs
@@ -1,9 +1,8 @@
 ﻿namespace Kundera.Management.Users;
 
-public sealed record UserResponse(Guid Id, IEnumerable<string> Usernames)
+public sealed record UserResponse(Guid Id, IEnumerable<string> Usernames, string Status)
 {
-    public string Status { get; set; }
     public IEnumerable<Guid> UserGroups { get; set; } = Array.Empty<Guid>();
 
-    public IEnumerable<string> Roles { get; set; } = Array.Empty<string>();
+    public IEnumerable<string>? Roles { get; set; } = Array.Empty<string>();
 }
