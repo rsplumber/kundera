5c436c5b64747131c7d4672e390d95df247ab77d
Aliasghar Sharifian(Plumber) <sha.a.wf@gmail.com>
2023-11-21 12:10:23 +0330

    feature: delete old sessions when password changed added.

--- Application/Auth/Credentials/Password/Change/CredentialChangePasswordCommand.cs
+++ Application/Auth/Credentials/Password/Change/CredentialChangePasswordCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Auth.Credentials;
 using Core.Auth.Credentials.Exceptions;
+using Core.Auth.Sessions;
 using Core.Users;
 using Core.Users.Exception;
 using KunderaNet.Authorization;
@@ -21,24 +22,32 @@ internal sealed class CredentialChangePasswordCommandHandler : ICommandHandler<C
     private readonly ICurrentUserService _currentUserService;
     private readonly IUserRepository _userRepository;
     private readonly ICredentialRepository _credentialRepository;
+    private readonly ISessionRepository _sessionRepository;
 
-    public CredentialChangePasswordCommandHandler(ICredentialRepository credentialRepository, ICurrentUserService currentUserService, IUserRepository userRepository)
+    public CredentialChangePasswordCommandHandler(ICredentialRepository credentialRepository, ICurrentUserService currentUserService, IUserRepository userRepository, ISessionRepository sessionRepository)
     {
         _credentialRepository = credentialRepository;
         _currentUserService = currentUserService;
         _userRepository = userRepository;
+        _sessionRepository = sessionRepository;
     }
 
     public async ValueTask<Unit> Handle(CredentialChangePasswordCommand command, CancellationToken cancellationToken)
     {
-        var user= await _userRepository.FindAsync(_currentUserService.User().Id, cancellationToken);
+        var user = await _userRepository.FindAsync(_currentUserService.User().Id, cancellationToken);
         if (user is null) throw new UserNotFoundException();
-        if(user.Usernames.All(s => s != command.Username)) throw new CredentialNotFoundException(); 
+        if (user.Usernames.All(s => s != command.Username)) throw new CredentialNotFoundException();
         var credentials = await _credentialRepository.FindByUsernameAsync(command.Username, cancellationToken);
         var credential = credentials.FirstOrDefault(credential => credential.Password.Check(command.Password));
         if (credential is null) throw new CredentialNotFoundException();
         credential.ChangePassword(command.Password, command.NewPassword);
         await _credentialRepository.UpdateAsync(credential, cancellationToken);
+        var sessions = await _sessionRepository.FindByCredentialIdAsync(credential.Id, cancellationToken);
+        foreach (var session in sessions)
+        {
+            await _sessionRepository.DeleteAsync(session.Id, cancellationToken);
+        }
+
         return Unit.Value;
     }
 }
