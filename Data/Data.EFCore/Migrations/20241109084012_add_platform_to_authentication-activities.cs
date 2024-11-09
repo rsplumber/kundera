2a2532f27dfe43abac312fab72220244669735c8
Aliasghar Sharifian(Plumber) <sha.a.wf@gmail.com>
2024-11-09 12:11:27 +0330

    feature: migrations added

---
+++ Data/Data.EFCore/Migrations/20241109084012_add_platform_to_authentication-activities.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_platform_to_authenticationactivities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Platform",
+                table: "authentication_activities",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Platform",
+                table: "authentication_activities");
+        }
+    }
+}
